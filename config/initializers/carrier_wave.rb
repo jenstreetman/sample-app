07bf66f891fd9b72d15697f425dfcd168287bc78
Jen <jenstreetman@gmail.com>
2019-02-01 12:22:29 -0700

    Fix carrier wave settings

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,8 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJPPCW5NTWAST5LBA
-'],
-      :aws_secret_access_key => ENV['jSdnE0tTwyVjuvtZcGDNIQ2F1tcFhwvgEviEGzS 
-']
+      :aws_access_key_id     => ENV['AKIAJPPCW5NTWAST5LBA'],
+      :aws_secret_access_key => ENV['jSdnE0tTwyVjuvtZcGDNIQ2F1tcFhwvgEviEGzS']
     }
     config.fog_directory     =  ENV['jenstreetman-sample-app']
   end

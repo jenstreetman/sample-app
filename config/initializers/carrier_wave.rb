ed1c1a354dd67272b70b321d7ff5645fce3b9504
Jen <jenstreetman@gmail.com>
2019-02-06 15:07:09 -0700

    Added omit default port = true

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,7 +6,8 @@ if Rails.env.production?
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :region                => ENV['S3_REGION']
+      :region                => ENV['S3_REGION'],
+      :omit_default_port     => true
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end

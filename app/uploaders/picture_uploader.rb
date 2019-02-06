fc74224d429afc57218523ad120d4e5a644954c4
Jen <jenstreetman@gmail.com>
2019-02-06 09:29:24 -0700

    Remove a line in picture-uploader.rb file

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,7 +1,6 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
-  storage :file
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?

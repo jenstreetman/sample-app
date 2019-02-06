7e862cdcbc86423a64075af140f3b1f85b043b8d
Jen <jenstreetman@gmail.com>
2019-02-06 09:18:50 -0700

    Add back environment variables

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,10 +4,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => 'AKIAJPPCW5NTWAST5LBA',
-      :aws_secret_access_key => 'jSdnE0tTwyVjuvtZcGDNIQ2F1tcFhwvgEviEGzS',
-      :region                => 'us-east-1'
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region                => ENV['S3_REGION']
     }
-    config.fog_directory     =  'jenstreetman-sample-app-2'
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
